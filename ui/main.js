8254dd46d8bc3a52ec681be017267c714d8d7372
quantumbit1 <47204969+quantumbit1@users.noreply.github.com>
2020-01-09 18:07:37 +0530

    Add files via upload
    This file is used to execute client side java script

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+
+var element =  document.getElementById('main_text');
+
+element.innerHTML='REPLACED TEXT';
+
+var img =  document.getElementById('madi');
+
+img.onclick=function(){
+    img.style.marginLeft ='100px';
+};
